99bd91f468b6cbccf54e7543127e50222331ba6f
Evan Merritt <109871289+MrMerrittx702@users.noreply.github.com>
2022-09-05 17:16:20 -0500

    Create TriangleArea.java

---
+++ TriangleArea.java
@@ -0,0 +1,11 @@
+//Calculate the area of a triangle
+
+//The area of a triangle is equal to 1/2 base time height. A = 1/2bh
+
+//Write a program that accepts values for base and height and prints the area
+
+
+//The value of the base is equal to 14.5 m
+//The value of the height should be equal 6.99 m
+
+//hint the number 5 is what data type? The number 5.0 is what data type? What is 1/5 in java?
